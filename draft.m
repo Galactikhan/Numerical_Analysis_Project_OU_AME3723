becc72f6016cca01a18aa21b82ef71eb3ce083f1
Galactikhan <116388190+Galactikhan@users.noreply.github.com>
2022-11-18 16:28:50 -0600

    Update draft.m
    Bisection with the project's function inputed

--- draft.m
+++ draft.m
@@ -10,9 +10,13 @@ if Fa*Fb >0
 else
     disp('iteration       a     b       (xNs)Solution  f(xNs)    Tolerance')
     for i=1:imax
+    
         xNS=(a+b)/2;
         toli=(b-a)/2;
+        
         FxNS=F(xNS);
+        
+        
         fprintf('%5i %11.6f %11.6f %11.6f %11.6f %11.6f\n',i,a,b,xNS,FxNS,toli);
         if toli<tol
             break
