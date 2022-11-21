f1ac1352fbceaf4fdfd81db16924b98fa6aebc58
Galactikhan <116388190+Galactikhan@users.noreply.github.com>
2022-11-21 14:10:53 -0600

    Update BisecAllRoots.m
    I left off at the first sign change interval; need to find the 2 values that trigger the if statement and use them for the Hundredths Sub-Interval.

--- BisecAllRoots.m
+++ BisecAllRoots.m
@@ -1,73 +1,25 @@
-function m = BisecAllRoots(f, a, b, TolMax)
-
-disp('Bisection Method');
-
-% Evaluate both ends of the interval
-
-y1 = feval(f, a);
-
-y2 = feval(f, b);
-
-i = 0;
-
-% Display error and finish if signs are not different
-
-if y1 * y2 > 0
-
-disp('Have not found a change in sign. Will not continue...');
-
-m = 'Error'
-
-return
-
+%%%%%%%%%%%%%%%%%%%
+% Define Function %
+%%%%%%%%%%%%%%%%%%%
+% R will be the output, and is a matrix composed
+% of the function, points a & b, and maximum tolerance.
+function R = BisecAllRoots(fun, a, b, TolMax)
+%
+%
+% Set Sub-Interval
+SubI1 = 10;
+h1 = abs((b-a)/SubI1);
+%
+RSub1 = zeros(1;SubI1)
+%
+for i = 1:(SubI1/2);
+    RSub1(i) = a + h;
+   for i = (SubI1/2):SubI1;
+       RSub1(i) = b - h;       
+   end
 end
-
-% Work with the limits modifying them until you find
-
-% a function close enough to zero.
-
-disp('Iter a b x0');
-
-while (abs(b - a) >= TolMax)
-
-i = i + 1;
-
-% Find a new value to be tested as a root
-
-m = (b + a)/2;
-
-y3 = feval(f, m);
-
-if y3 == 0
-
-fprintf('Root at x = %f \n\n', m);
-
-return
-
-end
-
-fprintf('%2i \t %f \t %f \t %f \n', i-1, a, b, m);
-
-% Update the limits
-
-if y1 * y3 > 0
-
-a = m;
-
-y1 = y3;
-
-else
-
-b = m;
-
-end
-
-end
-
-% Show the last approximation considering the tolerance
-
-w = feval(f, m);
-
-fprintf('\n x = %f produces f(x) = %f \n %i iterations\n', m, y3, i-1);
-
-fprintf(' Approximation with tolerance = %f \n',TolMax);
+for j = 1:SubI1-1;
+    
+if F(RSub1(j))*F(j+1) < 0 
+    disp(' Sign change detected in Sub-Interval ');
+    
