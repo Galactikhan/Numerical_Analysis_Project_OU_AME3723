1b9e760a2d4ff1beecfa6231336ad62e31d83add
Galactikhan <116388190+Galactikhan@users.noreply.github.com>
2022-11-22 04:03:29 -0600

    Update fun.m

--- fun.m
+++ fun.m
@@ -1,2 +1,10 @@
+%%%%%%%%%%%
+% Read ME %
+%%%%%%%%%%%
+% This defines the desired function for the BisecAllRoots.m
+% program, in this case:
+%                          f(x) = x^4-5.5*x^3-7.2*x^2+43*x+36
+% and can be altered for any other given function to run within
+% BisecAllRoots.m; to determine the function's roots.
 function y = fun(x)
 y =x^4-5.5*x^3-7.2*x^2+43*x+36;
